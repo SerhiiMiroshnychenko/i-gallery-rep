64718a2928260eb42107813755ee9eadadb893fa
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-06-01 18:09:28 +0300

    Show search value in console

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log (e.target[0].value);
+}
+
 const App = () => {
   return (
     <div className="App">
       <Header title="Ї-gallery" />
-      <Search />
+      <Search handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({handleSubmit}) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
-            <Row>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
               <Col xs={8}>
                 <Form.Control placeholder="Пошук нового зображення..." />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">ПОШУК</Button>
               </Col>
-            </Row>
+            </Form.Row>
           </Form>
         </Col>
       </Row>
