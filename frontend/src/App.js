26d939c642507888219f2312665a3f101a6f07be
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-06-02 09:42:40 +0300

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-
-const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log (e.target[0].value);
-}
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const App = () => {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
   return (
     <div className="App">
       <Header title="Ї-gallery" />
-      <Search handleSubmit={handleSearchSubmit} />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
